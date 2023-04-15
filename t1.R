bb8dd5383c7e7efbdb073bb0ddbf2dc394c8a2ff
Miguel Chen <1190897@isep.ipp.pt>
2023-04-15 17:59:25 +0100

    commit update do codigo

--- t1.R
+++ t1.R
@@ -66,34 +66,16 @@ boxplot(dados1c,main="Temperatura do motor nas bombas no dia 2013-08-04",xlab="B
 dados1di <- subset(dados1, format(TEMPO_POSIX, "%Y-%m") == "2014-03")
 View(dados1di)
 
-#numero de linhas do dataframe
-nlinha <- nrow(dados1di)
-# Definindo o tamanho do grupo
-n <- round(nlinha/31)
-
-# Calculando a média em cada grupo de n linhas para bomba1
-mediasb1 <- list()
-for (i in seq(1, nrow(dados1di), by=n)) {
-  grupo <- dados1di[i:(i+n-1),]
-  media_grupo <- mean(grupo$bbl.d.2)
-  mediasb1 <- append(mediasb1, media_grupo)
-}
-
-# Calculando a média em cada grupo de n linhas para bomba2
-mediasb2 <- list()
-for (i in seq(1, nrow(dados1di), by=n)) {
-  grupo <- dados1di[i:(i+n-1),]
-  media_grupo <- mean(grupo$bbl.d.5)
-  mediasb2 <- append(mediasb2, media_grupo)
-}
+total_barris1 <- aggregate(dados1di$bbl.d.2, list(format(dados1di$TEMPO_POSIX, "%j")), sum)
+total_barris2 <- aggregate(dados1di$bbl.d.5, list(format(dados1di$TEMPO_POSIX, "%j")), sum)
 
 bomba1 <- unlist(mediasb1)
 bomba2 <- unlist(mediasb2)
-counts <- rbind(bomba1,bomba2)
+counts <- rbind(total_barris1$x,total_barris2$x)
 
 #boxplot
-barplot(counts, main="Numero de Barris produzidos no mes de marco em 2014",
-        xlab="Numero de Barris", col=c("darkblue","red"),beside=TRUE)
+barplot(counts, main="Numero de Barris produzidos no mes de marco em 2014",ylab= "Numero de barris",
+        xlab="Dias", col=c("darkblue","red"),beside=TRUE)
 
 
 ##ii Em que mês a bomba 1 extraiu mais barris de petróleo?
@@ -114,7 +96,7 @@ mes_maior <- total_barris[which.max(total_barris$x), 1]
 
 ##iii Extraiu-se uma amostra aleatória de dias entre os dias 1-6-2013 e 31-5-2014 usando as
 ##seguintes instruções
- 
+
 
 ## [1] 78 362 66 156 277 41 339 169 272 268
 #Sendo que os dias foram numerados por ordem crescente, ou seja, 1
@@ -123,37 +105,32 @@ mes_maior <- total_barris[which.max(total_barris$x), 1]
 
 
 set.seed(300)
-dias_amostra <- sample(1:365,10)
+dias_amostra <- sample(1:365, 10)
 
-date <- as.Date(1, origin = as.Date("2013-06-01"))
+data_inicial <- as.Date("2013-06-01")
+
+datas_amostra <- data_inicial + dias_amostra  - 1
+print(datas_amostra)
+dt <- subset(dados1, as.numeric(as.Date(TEMPO_POSIX)) %in% datas_amostra)
+
 #criar subset com os dados entre datas 2013-06-01 a 2014-05-31
-dadosD4 <- subset(dados1,as.Date(TEMPO_POSIX) >= as.Date("2013-06-01") & as.Date(TEMPO_POSIX) <= as.Date("2014-05-31") ,origin= as.Date("2013-06-01"))
+dadosD4 <- subset(dados1,as.Date(TEMPO_POSIX) == as.Date(datas_amostra) ,origin= as.Date("2013-06-01"))
 
 #dadosD5 <- subset(dadosD4,as.Date(TEMPO_POSIX) = as.Date(1:364, origin = as.Date("2013-06-01")))
 View(dadosD4)
 
-#criar subset convertendo as datas do dadsoD4 no formato 1,2,3,4 e tirando aquelas que nao estao no dias_amostras
-dados_amostra <- subset(dadosD4,format(TEMPO_POSIX, "%j" ) %in% dias_amostra)
-View(dados_amostra)
-
 #calcula total de barris para cada bomba para cada dia
-total_barris1 <- aggregate(dados_amostra$bbl.d.2, list(format(dados_amostra$TEMPO_POSIX, "%j")), sum)
-total_barris2 <- aggregate(dados_amostra$bbl.d.5, list(format(dados_amostra$TEMPO_POSIX, "%j")), sum)
+total_barris1 <- aggregate(dadosD4$bbl.d.2, list(format(dadosD4$TEMPO_POSIX, "%j")), sum)
+total_barris2 <- aggregate(dadosD4$bbl.d.5, list(format(dadosD4$TEMPO_POSIX, "%j")), sum)
 
-# Boxplot para a produção da Bomba 1
-boxplot(total_barris1$x,
-        main = "Produção diária da Bomba 1 (amostra aleatória)",
+classes=c("total_barris1","total_barris2")
+# Boxplot para a produção da Bomba 1 e 2
+boxplot(total_barris1$x,total_barris2$x,
+        main = "Produção diária da Bomba 1 e bomba2 (amostra aleatória)",
         ylab = "Barris produzidos",
         ylim = c(0, max(total_barris1$x)),
-        col = "lightblue")
-
-# Boxplot para a produção da Bomba 2
-boxplot(total_barris2$x,
-        main = "Produção diária da Bomba 2 (amostra aleatória)",
-        ylab = "Barris produzidos",
-        ylim = c(0, max(total_barris2$x)),
-        col = "lightgreen")
-
+        names=classes,
+        col=c(4,2))
 
 
 ##iv Utilize as amostras aleatórias da alínea anterior para efetuar um teste de hipóteses que
@@ -173,15 +150,17 @@ library(BSDA)
 #H1:nao segue normalidade
 Shap_t <- shapiro.test(total_barris2$x)
 Shap_t$p.value
-#sendo p=0.0197 < alfa de 0,05 entao rejeita-se H0 e logo nao tem dist normal
+#sendo p=0.00102 < alfa de 0,05 entao rejeita-se H0 e logo nao tem dist normal
 
+
+##t.test(total_barris1$x, total_barris2$x, paired=TRUE, var=TRUE,alternative="greater")
 
 diff_bomba <- total_barris1$x - total_barris2$x
 
 #skewness verifica se tem simetria
 skewness(diff_bomba)
 
-# value= 1.524 > 1 logo nao e simetrico
+# value= 1.664474 > 1 logo nao e simetrico
 
 
 # Hipoteses:  h0 dif_bomba =0
@@ -189,7 +168,7 @@ skewness(diff_bomba)
 #teste de sinal uma vez que as amostras nao tem dist normal nem sao simetricos
 signt <- SIGN.test(diff_bomba,md=0,alternative = "greater") 
 signt$p.value
-#p value= 0.0078125< alfa de 0.05, rejeitamos assim H0.Ha evidencia de que bomba1 teve media de producao diaria superior a bomba 2 de ordem significativa
+#p value= 0.00195< alfa de 0.05, rejeitamos assim H0.Ha evidencia de que bomba1 teve media de producao diaria superior a bomba 2 de ordem significativa
 
 
 ##v Confirme se a decisão obtida no teste da alínea anterior corresponde à “realidade”.
@@ -197,11 +176,13 @@ signt$p.value
 #confirma-se
 
 
-##Ex2 Pretende-se comparar a precisão de 6 algoritmos de Machine Learning: SVM, DT, KN, RF, ML e GB. Para
+##2 Pretende-se comparar a precisão de 6 algoritmos de Machine Learning: SVM, DT, KN, RF, ML e GB. Para
 ##o efeito calculou-se a precisão de cada algoritmo sobre 10 conjuntos de dados: D1, D2, ..., D10. Os dados
 ##encontram-se guardados no ficheiro DADOS2.csv
 
+
 ##a
+
 
 library(readr)
 library(Hmisc)
@@ -217,6 +198,7 @@ rcorr(as.matrix(dados2[3:8]),type="pearson")
 #proximos de -1 estao negativamente fortemente correlacionadas 
 #proximo de 0 estao fracamente correlacionadas
 
+#C
      #SVM   DT   KN   RF   ML   GB
 #SVM 1.00 0.26 0.64 0.47 0.71 0.86
 #DT  0.26 1.00 0.43 0.88 0.62 0.21
@@ -225,9 +207,40 @@ rcorr(as.matrix(dados2[3:8]),type="pearson")
 #ML  0.71 0.62 0.85 0.57 1.00 0.72
 #GB  0.86 0.21 0.75 0.32 0.72 1.00
 
-# Positivamente fortemente correlacionadas: SVM-KN SVM-ML SVM-GB DT-RF DT-ML KN-ML KN-GB RF-ML ML-GB
-# Negativamente fortemente correlacionadas: N/A
-# Fracamente correlacionadas: SVM-DT SVM-RF DT-KN DT-GB KN-RF RF-GB 
+#P
+      #SVM    DT     KN     RF     ML     GB    
+#SVM        0.4646 0.0474 0.1747 0.0211 0.0013
+#DT  0.4646        0.2102 0.0008 0.0535 0.5552
+#KN  0.0474 0.2102        0.1569 0.0017 0.0124
+#RF  0.1747 0.0008 0.1569        0.0841 0.3610
+#ML  0.0211 0.0535 0.0017 0.0841        0.0186
+#GB  0.0013 0.5552 0.0124 0.3610 0.0186    
+
+# Positivamente fortemente correlacionadas: (C>0.7)     
+# Negativamente fortemente correlacionadas: (C<-0.7)
+# Fracamente correlacionadas: (-0.3<C<0.3) 
+# Positivamente moderadamente correlacionadas:(0.3<C<0.7)   
+# Negativamente moderadamente correlacionadas:(-0.3>C>-0.7)
+
+#cor SVM/ML é forte(0.71) (positiva e proxima de 1 e significativa p(0.0211)<alfa(0.05))
+#cor SVM/GB é forte(0.86) (positiva e proxima de 1 e significativa p(0.0013)<alfa(0.05))
+#cor DT/RF é forte(0.88) (positiva e proxima de 1 e significativa p(0.0008)<alfa(0.05))
+#cor KN/ML é forte(0.85) (positiva e proxima de 1 e significativa p(0.0017)<alfa(0.05))
+#cor KN/GB é forte(0.75) (positiva e proxima de 1 e significativa p(0.0124)<alfa(0.05))
+#cor ML/GB é forte(0.72) (positiva e proxima de 1 e significativa p(0.0186)<alfa(0.05))
+
+#cor SVM/DT é fraca(0.26) (positiva e proxima de 0 e nao significativa p(0.4646)>alfa(0.05))
+#cor SVM/DT é fraca(0.21) (positiva e proxima de 0 e nao significativa p(0.5552)>alfa(0.05))
+
+#cor SVM/KN é moderada(0.64) (positiva entre 0.3 e 0.7 e significativa p(0.0474)<alfa(0.05))
+#cor DT/ML é moderada(0.62) (positiva entre 0.3 e 0.7 e nao significativa p(0.0535)>alfa(0.05))
+#cor RF/ML é moderada(0.57) (positiva entre 0.3 e 0.7 e nao significativa p(0.0841)>alfa(0.05))
+#cor SVM/RF é moderada(0.47) (positiva entre 0.3 e 0.7 e nao significativa p(0.1747)>alfa(0.05))
+#cor DT/KN é moderada(0.43) (positiva entre 0.3 e 0.7 e nao significativa p(0.2102)>alfa(0.05))
+#cor KN/RD é moderada(0.48) (positiva entre 0.3 e 0.7 e nao significativa p(0.1569)>alfa(0.05))
+#cor RF/GB é moderada(0.32) (positiva entre 0.3 e 0.7 e nao significativa p(0.3610)>alfa(0.05))
+
+
 
 ##b
 
@@ -270,91 +283,97 @@ grupos <- factor (c(rep("SVM",10),rep("DT",10),rep ("KN",10),rep ("RF",10),rep (
 kruskal.test(dados_numericos,grupos)
 #como pvalue = 0,3335 > 0.05 nao rejeita-se H0 nao ha diferencas significativas entre as precisoes de algoritmos
 
+
 ##c como pvalue = 0,3335 > 0.05 nao rejeita-se H0 nao ha diferencas significativas entre as precisoes de algoritmos
 
 
-#3 O ficheiro DADOS3.csv contém dados de 4 variáveis (aceleração, número de cilindros, peso e
-#potência) de 99 viaturas escolhidas aleatoriamente:
+
+##3 O ficheiro DADOS3.csv contém dados de 4 variáveis (aceleração, número de cilindros, peso e
+##potência) de 99 viaturas escolhidas aleatoriamente:
 
 
-#a Divida as 99 viaturas em três grupos: viaturas com 4 cilindros, com 6 e com 8. Existirá diferenças
-#significativas na aceleração entre os três grupos?
+##a Divida as 99 viaturas em três grupos: viaturas com 4 cilindros, com 6 e com 8. Existirá diferenças
+##significativas na aceleração entre os três grupos?
+
+
+#ler dados de "DADOS3.CSV" para dataframe dados3
 
 
 dados3 <- read.csv("DADOS3.csv")
 View(dados3)
 
-
 aceleracao <- dados3$Acceleration
 cilindros <- dados3$Cylinders
 
-
 aceleracao_4c <- aceleracao[cilindros == 4]
-length(aceleracao_4c)  #lilith
+length(aceleracao_4c)  #lilith pk n>30
 aceleracao_6c <- aceleracao[cilindros == 6]
-length(aceleracao_6c)  #shapiro test
+length(aceleracao_6c)  #shapiro test pk n<30
 aceleracao_8c <- aceleracao[cilindros == 8]
-length(aceleracao_8c)  #lilith
+length(aceleracao_8c)  #lilith pk n>30
 
+#H0:segue normalidade
+#H1:nao segue normalidade
 
+#verifica dist normal
 Shap_t <- shapiro.test(aceleracao_6c)
 Shap_t$p.value
-
-#rejeita h0 not normal
+#sendo p=0.036 < alfa de 0,05 entao rejeita se H0 e logo nao tem dist normal
 
 Lill_t <- lillie.test(aceleracao_4c)
 Lill_t$p.value
- 
-#nao se rejeita h0
+#sendo p=0.289 >alfa de 0,05 entao nao se rejeita H0 e logo tem dist normal
 
 Lill_t <- lillie.test(aceleracao_8c)
 Lill_t$p.value
-
-#nao se rejeita h0
-
-
+#sendo p=0.780 >alfa de 0,05 entao nao se rejeita H0 e logo tem dist normal
 
 dados_numericos <-c(aceleracao_4c,aceleracao_6c,aceleracao_8c)
-
-
 grupos <- factor (c(rep("4",51),rep("6",17),rep ("8",31)))
 
+#teste kruskal uma vez uqe tem mais que 2 amostras independentes
 kruskal.test(dados_numericos,grupos)
+#como pvalue =2.79e-11 < 0.05 rejeita-se H0  ha diferencas significativas na aceleração entre os três grupos
 
 
+##bSupondo que a aceleração é a variável dependente e as restantes variáveis são independentes:
+##i Encontre o modelo de regressão linear. N.B. considere a variável número de cilindros uma
+##variável “Dummy”.
 
-#bSupondo que a aceleração é a variável dependente e as restantes variáveis são independentes:
-#i Encontre o modelo de regressão linear. N.B. considere a variável número de cilindros uma
-#variável “Dummy”.
 
-#y acele x1 weight horse factor cylin
-
+#y= acele x1= weight x2= horse  x3=factor x4=cylin
 
 colnames(dados3) <- c("Acceleration","Cylinders","Weight","Horsepower")
-
 dados3$Cylinders <- factor(dados3$Cylinders)
 
-reg3 <- lm(dados3$Acceleration ~ dados3$Weight + dados3$Horsepower + dados3$Cylinders)
-
+reg3 <- lm(dados3$Acceleration ~ dados3$Cylinders + dados3$Weight + dados3$Horsepower)
 reg3
 
 summary(reg3)
 
 
+# para cilindro 4 -> x1=0 entao y(chapeu)= 13.749056 + 0.003151x2 - 0.057381x3
+# para cilindro 6 -> x1=1 entao y(chapeu)= 13.749056 -1.515998 + 0.003151x2 - 0.057381x3
+# para cilindro 8 -> x1=2 entao y(chapeu)= 13.749056 -4.867451 * 2 + 0.003151x2 - 0.057381x3
+
+
 #ii Use o modelo encontrado na alínea anterior para estimar a aceleração de uma viatura com:
 #um peso de 2950 kg, potência de 100 Hp e 4 cilindros.
 
-peso <- 2950
-potencia <- 100
-num_cilindros <- "4" # Note que estamos usando a string "4" como valor para a variável categórica num_cilindros
+#y(chapeu) = 13.749056 + 0.003151*2950 - 0.057381*100
+
+y <- 13.749056 + 0.003151*2950 - 0.057381*100
+y
+#y estimado e de 17.30641 para aceleracao
 
 # Criar um novo data frame com os valores das variáveis independentes
-nova_viatura <- data.frame(Cylinders = num_cilindros, Weight = peso , Hosepower = potencia)
+nova_viatura <- data.frame(Cylinders = 4, Weight = 2950 , Hosepower = 100)
 
-estimativa_aceleracao <- predict(reg3, nova_viatura)
+estimativa_aceleracao <- predict(reg3, nova_viatura )
 
 print(estimativa_aceleracao)
 
 
 plot (fitted(reg3), residuals(reg3), xlab="Val. Ajustados", ylab="Residuos")
 abline(h=0)
+
