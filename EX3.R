6a42b3710d3af488cf4dedb2b9b5d817386c3135
Miguel Chen <1190897@isep.ipp.pt>
2023-06-15 17:31:23 +0000

    EX3.R edited online with Bitbucket

--- EX3.R
+++ EX3.R
@@ -1,6 +1,11 @@
-
-
 #EX3
+
+set.seed(42)
+minmaxnorm <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+data.norm <- as.data.frame(lapply(dataset, minmaxnorm))
 
 
 RMSE <- function(test, predicted) {
@@ -8,13 +13,13 @@ RMSE <- function(test, predicted) {
 }
 
 
-set.seed(123)
 index <- sample(1:nrow(dataset), 0.7 * nrow(dataset))
-data.train <- normalized_dataset[index, ]
-data.test <- normalized_dataset[-index, ]
 
-train_labels <- normalized_dataset[index,"gender"]
-test_labels <- normalized_dataset[-index,"gender"]
+data.train <- data.norm[index, -which(names(data.norm) == "gender")]
+data.test <- data.norm[-index,-which(names(data.norm) == "gender")]
+
+train_labels <- data.norm[index,"gender"]
+test_labels <- data.norm[-index,"gender"]
 
 k <- c()
 rmse <- c()
@@ -42,7 +47,7 @@ plot(resNeigh$k,
        'orangered1',
        'steelblue4'
      ))
-
+#k=31 rmse=0.4839
 #accuracy
 
 k <- c()
@@ -53,7 +58,9 @@ for (i in seq(1, 50, 2)){
   
   cfmatrix <- table(test_labels,knn.pred)
   
+  
   accuracy <- c(accuracy, sum(diag(cfmatrix))/sum(cfmatrix))
+  
   
   k <- c(k,i)
 }
@@ -77,19 +84,16 @@ plot(
   )
 )
 
-#100%
+#k=33 accuracy=0.6366667
 
 
 
 #NN
 
-set.seed(123)
-index <- sample(1:nrow(normalized_dataset), 0.7 * nrow(normalized_dataset))
-data.train <- normalized_dataset[index, ]
-data.test <- normalized_dataset[-index, ]
-
-train_labels <- normalized_dataset[index,"gender"]
-test_labels <- normalized_dataset[-index,"gender"]
+set.seed(42)
+index <- sample(1:nrow(data.norm), 0.7 * nrow(data.norm))
+data.train <- data.norm[index,  ]
+data.test <- data.norm[-index, ]
 
 
 nn.model <- neuralnet(gender ~ ., data = data.train, hidden = numnodes <- 2,stepmax = 1e6 )
@@ -100,7 +104,7 @@ nn.pred <- compute(nn.model, data.test[, -1])
 nn.pred.gender <- minmaxdesnorm(nn.pred$net.result, dataset$gender)
 test.gender  <- minmaxdesnorm(data.test$gender, dataset$gender)
 
-RMSE(nn.pred.gender, test.gender)
+#RMSE(nn.pred.gender, test.gender)
 
 cfmatrix2 <- table(nn.pred.gender, test.gender )
 nn_accuracy <- sum(diag(cfmatrix2)) / sum(cfmatrix2) * 100
@@ -115,32 +119,39 @@ folds <- sample(1:cvf, nrow(dataset), replace = TRUE)
 table(folds)
 
 accuracy <- matrix(nrow = cvf, ncol = 2)
-k <- 20
-ncols <- dim(dataset)[2]
+k <- 33
+cv.error <- matrix(nrow = cvf, ncol = 2)
 
 for (i in 1:cvf){
   
-  train.cv <- normalized_dataset[folds != i, ]
-  test.cv <- normalized_dataset[folds == i, ]
+  train.cv <- data.norm[folds != i, -which(names(data.norm) == "gender")]
+  test.cv <- data.norm[folds == i, -which(names(data.norm) == "gender")]
+  
+  train1.cv <- data.norm[folds != i, ]
+  test1.cv <- data.norm[folds == i, ]
   
   train_labels <- dataset[folds != i, "gender"]
   tst_labels <- dataset[folds == i, "gender"]
   
-  knn.pred <- knn(train=train.cv[,-ncols], test=test.cv[,-ncols], cl=train_labels, k) 
+  knn.pred <- knn(train=train.cv, test=test.cv, cl=train_labels, k) 
   cfmatknn <- table(tst_labels,knn.pred)
   
-  nn.model <- neuralnet(gender ~ ., data = train.cv, hidden = numnodes <- 2 )
+  nn.model <- neuralnet(gender ~ ., data = train1.cv, hidden = numnodes <- 2,stepmax = 1e6 )
   
   #plot(nn.model)
-  nn.pred <- compute(nn.model, test.cv[, -1])
+  nn.pred <- compute(nn.model, test1.cv[, -which(names(data.norm) == "gender")])
   
   nn.pred.gender <- minmaxdesnorm(nn.pred$net.result, dataset$gender)
-  test.gender  <- minmaxdesnorm(test.cv$gender, dataset$gender)
+  test1.cv$gender  <- minmaxdesnorm(test1.cv$gender, dataset$gender)
   
-  cfmatnn <- table(nn.pred.gender,test.gender)
+  RMSE(nn.pred.gender, test1.cv$gender)
   
-  accuracy[i, ] <- c( sum(diag(cfmatknn))/sum(cfmatknn),
-                      sum(diag(cfmatnn))/sum(cfmatnn)) 
+  cfmatnn <- table(nn.pred.gender,test1.cv$gender)
+    
+  accuracy[i, ] <- c( sum(diag(cfmatknn))/sum(cfmatknn)*100,
+                      sum(diag(cfmatnn))/sum(cfmatnn)*100) 
+  
+  
 }
 
 #accuracy
@@ -148,58 +159,65 @@ for (i in 1:cvf){
 apply(accuracy,2,mean)
 apply(accuracy,2,sd)
 
-
+# Media Knn = 0.7794
+# Desvio Knn = 0.0399
+# Ambos os modelos têm uma média de precisão em torno de 67% e desvio padrão baixo
+# O que indica consistência dos resultados ao longo das k-folds
+# E apresentam um desempenho semelhante no calculo da previsao do "Pro.level"
 
 
 #B
-# H0: modelos são significativamente iguais VS H1: modelos são significativamente diferentes
-# alfa = 5%
-#p-value = 6.34e-05
-# Como o p-value é inferior a alfa rejeitamos a hipótese nula. Ou seja, os modelos nao são significativamente iguais 
-wilcox.test(accuracy[, 1], accuracy[, 2],exact = FALSE)
+# H0: os modelos sÃ£o significativamente iguais 
+# H1: os modelos sÃ£o significativamente diferentes
+
+t.test(accuracy[, 1], accuracy[, 2])  #p-value = 3.849e-12
+# Como o p-value Ã© inferior a alfa (0.05) rejeitamos a hipÃ³tese nula. 
+# Ou seja, os modelos os modelos sÃ£o significativamente diferentes
 
 
-friedman.test(accuracy)
-#p-value = 0.001565
-
-#c
-
+#C
+parse_results <- function(m.conf) {
+  accuracy <- 100 * round((m.conf[1, 1] + m.conf[2, 2]) / sum(m.conf), 4)
+  recall = m.conf[1, 1] / (m.conf[1, 1] + m.conf[1, 2])
+  precision = m.conf[1, 1] / (m.conf[1, 1] + m.conf[2, 1])
+  f1 = (2 * precision * recall) / (precision + recall)
+  
+  message("accuracy: ", accuracy, "%")
+  message("Recall: ", recall)
+  message("precision: ", precision)
+  message("F1: ", f1)
+  
+  my_list <-
+    list(
+      "F1" = f1,
+      "precision" = precision,
+      "recall/sensitivity" = recall,
+      "accuracy" = accuracy
+    )
+  return(my_list)
+}
 ##K
+
+
 index <- sample(1:nrow(dataset), 0.7 * nrow(dataset))
-data.train <- normalized_dataset[index, ]
-data.test <- normalized_dataset[-index, ]
 
-train_labels <- normalized_dataset[index,"gender"]
-test_labels <- normalized_dataset[-index,"gender"]
+data.train <- data.norm[index, -which(names(data.norm) == "gender")]
+data.test <- data.norm[-index,-which(names(data.norm) == "gender")]
 
-knn.pred <- knn(train=data.train, test=data.test, cl=train_labels, k=10) 
+train_labels <- data.norm[index,"gender"]
+test_labels <- data.norm[-index,"gender"]
+
+knn.pred <- knn(train=data.train, test=data.test, cl=train_labels, k=33) 
 
 knn_cfmatrix <- table(test_labels,knn.pred)
-#knn_predictions <- as.factor(knn_model)
-#knn_cfmatrix <- table(knn_predictions, test_data$gender)
 
-# Calcular métricas para k-NN
-knn_accuracy <- sum(diag(knn_cfmatrix)) / sum(knn_cfmatrix)  # Accuracy
-knn_sensitivity <- knn_cfmatrix[2, 2] / sum(knn_cfmatrix[2, ])  # Sensitivity
-knn_specificity <- knn_cfmatrix[1, 1] / sum(knn_cfmatrix[1, ])  # Specificity
-knn_f1 <- 2 * knn_sensitivity * knn_specificity / (knn_sensitivity + knn_specificity)  # F1 score
-
-# Imprimir resultados
-cat("k-NN:\n")
-cat("Accuracy:", knn_accuracy, "\n")
-cat("Sensitivity:", knn_sensitivity, "\n")
-cat("Specificity:", knn_specificity, "\n")
-cat("F1 score:", knn_f1, "\n")
+knn_results <-  parse_results(knn_cfmatrix)
 
 # Modelo de Rede Neural
 
-index <- sample(1:nrow(normalized_dataset), 0.7 * nrow(normalized_dataset))
-data.train <- normalized_dataset[index, ]
-data.test <- normalized_dataset[-index, ]
-
-train_labels <- normalized_dataset[index,"gender"]
-test_labels <- normalized_dataset[-index,"gender"]
-
+index <- sample(1:nrow(data.norm), 0.7 * nrow(data.norm))
+data.train <- data.norm[index,  ]
+data.test <- data.norm[-index, ]
 
 nn.model <- neuralnet(gender ~ ., data = data.train, hidden = numnodes <- 2,stepmax = 1e6 )
 
@@ -213,14 +231,4 @@ RMSE(nn.pred.gender, test.gender)
 
 nn_cfmatrix <- table(nn.pred.gender, test.gender )
 
-# Calcular métricas para Rede Neural
-nn_accuracy <- sum(diag(nn_cfmatrix)) / sum(nn_cfmatrix)  # Accuracy
-nn_sensitivity <- nn_cfmatrix[2, 2] / sum(nn_cfmatrix[2, ])  # Sensitivity
-nn_specificity <- nn_cfmatrix[1, 1] / sum(nn_cfmatrix[1, ])  # Specificity
-nn_f1 <- 2 * nn_sensitivity * nn_specificity / (nn_sensitivity + nn_specificity)  # F1 score
-
-cat("Rede Neural:\n")
-cat("Accuracy:", nn_accuracy, "\n")
-cat("Sensitivity:", nn_sensitivity, "\n")
-cat("Specificity:", nn_specificity, "\n")
-cat("F1 score:", nn_f1, "\n")
+nn_results <- parse_results(nn_cfmatrix)
